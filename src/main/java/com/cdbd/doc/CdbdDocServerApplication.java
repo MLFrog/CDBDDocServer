cf4864974fc6f8202ca3f948b1d90347b2ea1cd7
Hee <ycross4008@gmail.com>
2024-12-22 18:39:45 +0900

    add init project

---
+++ src/main/java/com/cdbd/doc/CdbdDocServerApplication.java
@@ -0,0 +1,13 @@
+package com.cdbd.doc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdbdDocServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdbdDocServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cdbd/doc/CdbdDocServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdbd.doc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdbdDocServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
